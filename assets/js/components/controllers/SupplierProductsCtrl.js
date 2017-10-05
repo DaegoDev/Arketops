bbb2b73752d51b32cb9b4a7d35d9297423c64727
Jonnatan <jrios328@gmail.com>
2017-10-05 14:22:39 -0500

    Supplier products

---
+++ assets/js/components/controllers/SupplierProductsCtrl.js
@@ -0,0 +1,18 @@
+var arketops = angular.module('arketops');
+arketops.controller('SupplierProductsCtrl', ['$scope', '$log', '$state', '$stateParams',
+  '$ngConfirm', 'ProductSvc', 'StorageSvc',
+  function($scope, $log, $state, $stateParams, $ngConfirm, ProductSvc, StorageSvc) {
+    // Declaration of variables
+    $scope.supplier = JSON.parse(StorageSvc.get('supplierSelected', 'session'));
+    // console.log($scope.supplier);
+
+    ProductSvc.getByCompany({companyId: $scope.supplier.id})
+    .then((res) => {
+      console.log(res.data);
+      $scope.products = res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+]);
