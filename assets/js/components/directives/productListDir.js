b3678ed8cfbc385cb688fe133b27a7602e88943d
Daego <daegodev@gmail.com>
2017-10-27 05:06:59 -0500

    Delete confirmation on product

--- assets/js/components/directives/productListDir.js
+++ assets/js/components/directives/productListDir.js
@@ -17,13 +17,43 @@ arketops.controller('productListCtrl',
 
 function productListCtrl($scope, $log, $ngConfirm, AuthSvc, $state, StorageSvc, ProductSvc) {
 
-  //
+  // Flag function to know if the current elements belongs to the user.
   $scope.isPrivate = function () {
     return $scope.mode.toUpperCase() === 'OWNER';
   }
 
-  //
-  $scope.disable = function (product) {
+  // Function to ask confirmation to delete a product.
+  $scope.deleteConfirm = function (product) {
+    $ngConfirm({
+      theme: 'material',
+      title: "Eliminar producto",
+      content: "¿Desea eliminar el producto con código " + product.code + "?",
+      type: 'orange',
+      useBootstrap: false,
+      boxWidth: '450px',
+      typeAnimated: true,
+      buttons: {
+        confirm: {
+          text: "Aceptar",
+          btnClass: 'btn',
+          action: function () {
+            $scope.deleteProduct(product);
+          }
+        },
+
+        exit: {
+          text: "Cancelar",
+          btnClass: 'btn',
+          action: function () {
+
+          }
+        }
+      }
+    });
+  }
+
+  // Function to delete a product.
+  $scope.deleteProduct = function (product) {
     ProductSvc.delete(product.id)
     .then(function (res) {
       $scope.products.splice($scope.products.indexOf(product), 1);
