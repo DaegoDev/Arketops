a0b50b38ad01abc3c0232f4b653de8f44f9535e2
Jonnatan <jrios328@gmail.com>
2017-12-14 10:28:45 -0500

    Mailer

--- config/email.js
+++ config/email.js
@@ -8,15 +8,32 @@
 // }
 var nodemailer = require('nodemailer');
 
+// var transporter = nodemailer.createTransport({
+//     host: 'mail.arketops.com',
+//     port: '465',
+//     secure: true, // use TLS
+    // auth: {
+    //     user: 'app@arketops.com',
+    //     pass: 'app$2016'
+    // },
+//     requireTLS: false,
+//     logger: true,
+//     debug: true,
+//     tls: {
+//         rejectUnauthorized: false
+//     }
+// });
+
 var transporter = nodemailer.createTransport({
-    host: 'smtp.arketops.com',
-    port: '587',
-    secure: false, // use TLS
+    host: 'smtp.gmail.com',
+    port: '465',
+    secure: true, // use TLS
+    // service: 'Gmail',
     auth: {
-        user: 'app@arketops.com',
-        pass: 'app$2016'
+        user: 'arketops1@gmail.com',
+        pass: 'arketops123'
     },
-    requireTLS: true,
+    requireTLS: false,
     logger: true,
     debug: true,
     tls: {
@@ -24,23 +41,39 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+// var transporter = nodemailer.createTransport({
+//     sendmail: true,
+//     newline: 'unix',
+//     path: '/usr/sbin/sendmail'
+// });
+
 // var transporter = nodemailer.createTransport('smtps://app@arketops.com:app$2016@smtp.arketops.com')
 
 // verify connection configuration
 transporter.verify(function(error, success) {
    if (error) {
-     console.log(error);
-        // sails.log.error(error);
+        sails.log.error(error);
    } else {
-        sails.log.debug(success);
         sails.log.debug('Server is ready to take our messages');
    }
 });
 
 module.exports.email = {
   transporter: transporter,
+  // service: 'Gmail',
+  // auth: {
+  //     user: 'arketops1@gmail.com',
+  //     pass: 'arketops123'
+  // },
+  // service:'Webmail',
+  // auth: {
+  //     user: 'app@arketops.com',
+  //     pass: 'app$2016'
+  // },
   templateDir: "api/emailTemplates",
-  from: "app@arketops.com",
+  // from: "app@arketops.com",
+  from: 'arketops1@gmail.com',
+  // from: 'jonnatan@jonnatan-x441uv',
   testMode: false,
 };
 
