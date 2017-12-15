ad99a924cbc6cce060090344035b2cdfbe9d8dd0
Jonnatan <jrios328@gmail.com>
2017-12-15 11:46:59 -0500

    Order by in search Results

--- assets/js/components/controllers/ResultListCtrl.js
+++ assets/js/components/controllers/ResultListCtrl.js
@@ -1,25 +1,70 @@
 var arketops = angular.module('arketops');
-arketops.controller('ResultListCtrl', ['$scope', '$log', '$state', '$stateParams', 'CompanySvc', 'ProductSvc', 'StorageSvc',
- function($scope, $log, $state, $stateParams, CompanySvc, ProductSvc, StorageSvc) {
+arketops.controller('ResultListCtrl', ['$scope', '$log', '$state', '$stateParams', 'CompanySvc',
+'ProductSvc', 'StorageSvc', 'orderByFilter',
+ function($scope, $log, $state, $stateParams, CompanySvc, ProductSvc, StorageSvc, orderBy) {
   $scope.searchValue = StorageSvc.get('searchValue', 'session');
   $scope.filter = StorageSvc.get('filter', 'session');
 
   $scope.result = [];
   $scope.total = 0;
   $scope.flagProductHeader = true;
+  $scope.orderOptionsCompanies = {};
+  $scope.orderOptionsProducts = {};
 
   // Declaración de variables para la paginación.
   $scope.filteredResults = [];
   $scope.itemsPerPage = 20;
 
+  $scope.choicesOrderCompany = [
+    {
+      name: 'Seleccione',
+      value: -1
+    },
+    {
+      name: 'Nombre - Alfabeticamente descendente',
+      value: 1
+    },
+    {
+      name: 'Nombre - Alfabeticamente ascendente',
+      value: 2
+    }
+  ]
+  $scope.orderOptionsCompanies = {
+    choices: $scope.choicesOrderCompany,
+    selected: $scope.choicesOrderCompany[0]
+  }
+
+  $scope.choicesOrderProduct = [
+    {
+      name: 'Seleccione',
+      value: -1
+    },
+    {
+      name: 'Nombre - Alfabeticamente descendente',
+      value: 1
+    },
+    {
+      name: 'Nombre - Alfabeticamente ascendente',
+      value: 2
+    },
+    {
+      name: 'Precio - De menor a mayor',
+      value: 3
+    },
+    {
+      name: 'Precio - De mayor a menor',
+      value: 4
+    }
+  ]
+  $scope.orderOptionsProducts = {
+    choices: $scope.choicesOrderProduct,
+    selected: $scope.choicesOrderProduct[0]
+  }
+
   $scope.figureOutResultsToDisplay = function(currentPage) {
     var begin = ((currentPage - 1) * $scope.itemsPerPage);
     var end = begin + $scope.itemsPerPage;
-    // if (isSearchAll) {
-    //   $scope.filteredResults = $scope.result.companies.slice(begin, end);
-    // } else {
-      $scope.filteredResults = $scope.result.slice(begin, end);
-    // }
+    $scope.filteredResults = $scope.result.slice(begin, end);
   };
 
   $scope.changePage = function(currentPage) {
@@ -53,7 +98,7 @@ arketops.controller('ResultListCtrl', ['$scope', '$log', '$state', '$stateParams
         name: $scope.searchValue
       })
       .then((result) => {
-        // console.log(result.data);
+        console.log(result.data);
         $scope.result = result.data;
         $scope.total = $scope.result.length
         $scope.figureOutResultsToDisplay(1);
@@ -64,23 +109,41 @@ arketops.controller('ResultListCtrl', ['$scope', '$log', '$state', '$stateParams
         keyword: $scope.searchValue
       })
       .then((result) => {
-        $scope.result = result.data.companies.concat(result.data.products);
-        // $scope.result = result.data;
+
+        $scope.result = result.data.companies;
+        $scope.result.push('divider');
+        $scope.result = $scope.result.concat(result.data.products);
         $scope.total = $scope.result.length
         $scope.figureOutResultsToDisplay(1);
       })
   }
 
-  $scope.greenFlagToSubtittle = function (compProd) {
-    var greenFlag = false;
-    console.log(compProd.type);
-    console.log($scope.flagProductHeader);
-    if (compProd.type === 2 && $scope.flagProductHeader) {
-      greenFlag = true;
-      $scope.flagProductHeader = false;
+  $scope.orderCompanies = function () {
+    var value = $scope.orderOptionsCompanies.selected.value;
+    if (value === -1) {
+      return;
+    } else if (value === 1) {
+      $scope.result = orderBy($scope.result, 'name', false);
+    } else if (value === 2) {
+      $scope.result = orderBy($scope.result, 'name', true);
     }
-    // console.log(greenFlag);
-    return greenFlag;
+    $scope.figureOutResultsToDisplay(1);
+  }
+
+  $scope.orderProducts = function () {
+    var value = $scope.orderOptionsProducts.selected.value;
+    if (value === -1) {
+      return;
+    } else if (value === 1) {
+      $scope.result = orderBy($scope.result, 'name', false);
+    } else if (value === 2) {
+      $scope.result = orderBy($scope.result, 'name', true);
+    } else if (value === 3) {
+      $scope.result = orderBy($scope.result, 'price', false);
+    } else if (value === 4) {
+      $scope.result = orderBy($scope.result, 'price', true);
+    }
+    $scope.figureOutResultsToDisplay(1);
   }
 
 }]);
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -1,3 +1,4 @@
+require('events').EventEmitter.defaultMaxListeners = 0;
 /**
  * grunt/pipeline.js
  *
